da05ca4f60ae56ab339bc7cf8a37931c4fab71bf
zinchen-ko <zinchen_ko@mail.ru>
2023-12-17 17:56:52 +0300

    Fix config

--- src/main/java/com/soa/lab2soa/Lab2soaApplication.java
+++ src/main/java/com/soa/lab2soa/Lab2soaApplication.java
@@ -3,7 +3,10 @@ package com.soa.lab2soa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Lab2soaApplication extends SpringBootServletInitializer {
 
